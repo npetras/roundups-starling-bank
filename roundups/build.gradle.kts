d9cf9f355070db417d1a100e8f8152b1f7bd0347
Nicolas Petras <nicolaspetras@gmail.com>
2023-05-10 00:07:35 +0100

    config: clean-up of dependecies and addition of run command Added application plugin, so gradle run could be used to run the app removed all unused dependecies

--- roundups/build.gradle.kts
+++ roundups/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.nicolaspetras"
@@ -10,23 +11,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-
-    //	Lombok
-    compileOnly("org.projectlombok:lombok:1.18.26")
-    annotationProcessor("org.projectlombok:lombok:1.18.26")
-    testCompileOnly("org.projectlombok:lombok:1.18.26")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.26")
 
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.apache.commons:commons-text:1.10.0")
+
+    // logging
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
-
-    implementation("org.apache.commons:commons-text:1.10.0")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+application {
+    mainClass.set("com.nicolaspetras.roundups.Main")
 }
