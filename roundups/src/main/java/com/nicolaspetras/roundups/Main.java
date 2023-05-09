61cdb81bdda2a3b3cf9551b5027db570514481bc
Nicolas Petras <nicolaspetras@gmail.com>
2023-05-09 23:58:57 +0100

    refactor: RoundupsService and Main to use more functions General clean-up of the code, including: - separating code into reusable functions - removing old/redundant code - removing old/redundant imports

--- roundups/src/main/java/com/nicolaspetras/roundups/Main.java
+++ roundups/src/main/java/com/nicolaspetras/roundups/Main.java
@@ -1,54 +1,18 @@
 package com.nicolaspetras.roundups;
 
-import com.google.gson.Gson;
-import com.nicolaspetras.roundups.api.ApiCalls;
-import com.nicolaspetras.roundups.data.CurrencyAndAmount;
-import com.nicolaspetras.roundups.data.account.AccountsList;
-import com.nicolaspetras.roundups.data.request.SavingGoalRequestBody;
-import com.nicolaspetras.roundups.data.request.TopUpRequestBody;
-import com.nicolaspetras.roundups.data.response.CreateOrUpdateSavingsGoalResponse;
-import com.nicolaspetras.roundups.data.savings.SavingsGoalList;
-import com.nicolaspetras.roundups.data.transaction.Transaction;
-import com.nicolaspetras.roundups.data.transaction.TransactionFeed;
-
 import com.nicolaspetras.roundups.service.RoundupsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.nicolaspetras.roundups.api.ApiCalls.STARLING_API_BASE_URL;
-
-
 public class Main {
     static Logger log = LoggerFactory.getLogger(Main.class);
 
+    /**
+     * Runs the application
+     * @param args
+     */
     public static void main(String[] args) {
         RoundupsService appService = new RoundupsService();
         appService.runRoundupsApplication();
-    }
-
-    /**
-     * Retrieves a feed of the last week of transactions from the Starling Bank API using the account
-     * data (account UID and default category ID) also retrieved from the Starling Bank API. The Starling API
-     * returns the data in the form of JSON, which is deserialised into the application's Java classes using the
-     * GSON library.
-     *
-     * @return Transaction Feed of last week's transaction
-     */
-    private static TransactionFeed retrieveAndDeserialiseLastWeekTransactions() {
-        var gson = new Gson();
-        var apiCalls = new ApiCalls();
-
-        var accountsApiResponse = apiCalls.sendBasicGetRequest("https://api-sandbox.starlingbank.com/api/v2/accounts");
-        log.debug(accountsApiResponse);
-
-        AccountsList accounts = gson.fromJson(accountsApiResponse, AccountsList.class);
-
-        var transactionFeedsUri = "https://api-sandbox.starlingbank.com/api/v2/feed/account/"
-                + accounts.accounts()[0].accountUid() + "/category/" + accounts.accounts()[0].defaultCategory()
-                + "?changesSince=2020-01-01T12%3A34%3A56.000Z";
-        var transactionFeedsResponse = apiCalls.sendBasicGetRequest(transactionFeedsUri);
-        log.debug(transactionFeedsResponse);
-
-        return gson.fromJson(transactionFeedsResponse, TransactionFeed.class);
     }
 }
--- roundups/src/main/java/com/nicolaspetras/roundups/service/RoundupsService.java
+++ roundups/src/main/java/com/nicolaspetras/roundups/service/RoundupsService.java
@@ -3,6 +3,7 @@ package com.nicolaspetras.roundups.service;
 import com.google.gson.Gson;
 import com.nicolaspetras.roundups.api.ApiCalls;
 import com.nicolaspetras.roundups.data.CurrencyAndAmount;
+import com.nicolaspetras.roundups.data.account.Account;
 import com.nicolaspetras.roundups.data.account.AccountsList;
 import com.nicolaspetras.roundups.data.request.SavingGoalRequestBody;
 import com.nicolaspetras.roundups.data.request.TopUpRequestBody;
@@ -13,72 +14,108 @@ import com.nicolaspetras.roundups.data.transaction.TransactionFeed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 import static com.nicolaspetras.roundups.api.ApiCalls.STARLING_API_BASE_URL;
 
+/**
+ * Application class for the Round-ups Service, which GETs the relevant data from the Starling API on the
+ * customer Account and Transaction Feed, and uses that information to calculate round-ups on last week's transactions
+ * and then move the total round-up amount to a Round Up Savings Goal.
+ */
 public class RoundupsService {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
-    public static final String ACCESS_TOKEN = "Bearer eyJhbGciOiJQUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAA_21Ty27cMAz8lcLnMIhfWntvvfUH-gE0SWWF2JIhyUmDov9eeeVdx4vcPDPkkBTpv4UJoTgXOBtgmdxziOhHY18HtG_P5KbiqQjLkCLKskXpuhZeFL5A09UD9EQaWnypWHqlmCgFy5-5OJeqq1XddXX7VBiMmWjbqlkJJHKLjb_cyOJ_G9689UkTCPYNNDw0gIpLUDSUbdv2rT4NyTu6N7E5o1LNqa8rgXJQChpihKHWHXSc2BL1cJIqZaSxfhJJCHudGlUFpdIpS1MDfaX6NNKpxkqo4apcByY3y_oouVO4XFsFi5OcvSD_eBDi5_wgGBYbjTbij_xoQjwwG2D2qcmzsIl3kJUYkS6T3COXeHHehLQhMJbNu-EFx6wNOKKlrRNCz0DORu_G7Lsym-asNn7CaJwFp0Evlrd6tIToplvbMqHZsie0jFHOLKNEucNr2CQRE8IzJbiKN3zNnPFT5CZlsJlksAeBmfB188za_gnRow1Ia893GkZHafrdOxPg1md4ZLcs77QZb6Vy7QN1jfJCYuZ4AOEofXiT3AO-p1UEeHV7HwduG_XAXX2-Mnk4nZ79G4td_MZrF7MpXYSXURjS2PvVBIkxDbjMG5zxdibpd09XlI7Jef5S_sje6h7Zb_LBfdg7H2VtACi8P1Iz60x93el1FY9LLv79BwNQQVGhBAAA.EE8Y9FapdFUlFbDDy-u1s9vUaw0F85ocpQ3CIHxrMDC1Mj4sKfiVD026miHEyaohBBTiu19xkt78ZieiKsyafeLoUL0ZndkfHas-V3HDu89wOyGAJeKZ6VQ-3I4E68TJpxuGhcqpFrf6ibcT8Sq_ukiAMcQYvTx3b_coJ7jphErWqob1hVmc9P5rTPbwRwy76o7henSbguEEjyk1HltrrdC4a7JNXy0KFhc5iMLhr-bm6JOeH5DiBpMXFfEPYCBbuSbqA79ABgZDswETcY_LBl4pWy8mow2Ll3ceQN1jRHwZlQxHtWirc0WofyHcj5iAH7GNYBetI_AbwAODtrmEdESx4SSZ2JhbmVnVGVx706imIDai2uv6BDpYdYaZIaQqmOmezZT_sjOSNnLcNESMRRs8AUpyRqXjnTxhtKvQC3p3Otd3GpnXnvPAcbwd3F7hjW7mDHIXcUXj5SvPFmW8qodeF_sshIEEKIGpODqeu3VBaTUPhokKp68CIXwO2pHF4Gd_b4DZ-8P2Mh1hHQ_r-eHRRK_rktNVCPutqRRT4jcQQM5FiYyMC65SlcDWOzV4CkZOXLw2K5i6mByYjmyExvFMrOAhOyChaf53Jrb1Fx1jDVX_xDyVVPAuarRQRHM5UAEC_H1WZa2GtU-P2GvPcjHjC0VbYkpp4BXrJmmNeUo";
     public static final String ROUND_UP_SAVINGS_GOAL = "Round-up Savings";
-    private Gson gson;
-    private ApiCalls apiCalls;
+    private final Gson gson;
+    private final ApiCalls apiCalls;
 
     public RoundupsService() {
         this.gson = new Gson();
         this.apiCalls = new ApiCalls();
     }
 
+    /**
+     * Runs the Round-Ups application.
+     */
     public void runRoundupsApplication() {
-        var accountsApiResponse = apiCalls.sendBasicGetRequest(STARLING_API_BASE_URL + "/api/v2/accounts");
-        log.debug(accountsApiResponse);
+        Account primaryAccount = retrievePrimaryAccount();
+        var lastWeekTransactions = retrieveLastWeekTransactionFeed(primaryAccount.accountUid(),
+                primaryAccount.defaultCategory());
 
-        AccountsList accounts = gson.fromJson(accountsApiResponse, AccountsList.class);
-
-        var transactionFeedsUri = STARLING_API_BASE_URL + "/api/v2/feed/account/"
-                + accounts.accounts()[0].accountUid() + "/category/" + accounts.accounts()[0].defaultCategory()
-                + "?changesSince=2020-01-01T12%3A34%3A56.000Z";
-        var transactionFeedsResponse = apiCalls.sendBasicGetRequest(transactionFeedsUri);
-        log.debug(transactionFeedsResponse);
-
-        var lastWeekTransactions = gson.fromJson(transactionFeedsResponse, TransactionFeed.class);
         var totalRoundup = calculateTotalRoundup(lastWeekTransactions);
         System.out.println("Total round-up: " + totalRoundup);
-        var savingGoalsResponse = apiCalls.sendBasicGetRequest(STARLING_API_BASE_URL + "/api/v2/account/" + accounts.accounts()[0].accountUid() + "/savings-goals");
-        var savingsGoalList = gson.fromJson(savingGoalsResponse, SavingsGoalList.class);
+
+        var savingsGoalList = retrieveSavingsGoalList(primaryAccount.accountUid());
 
         if (totalRoundup > 0) {
             // if the savings goal already exists
             if (savingsGoalList.isSavingsGoalPresent(ROUND_UP_SAVINGS_GOAL)) {
                 var roundUpsSavingGoal = savingsGoalList.getSavingGoal(ROUND_UP_SAVINGS_GOAL);
-                var requestBody = new TopUpRequestBody(new CurrencyAndAmount("GBP", totalRoundup));
-                var addMoneyToSavingsGoalResponse = apiCalls.sendAddMoneyIntoSavingsGoalRequest(accounts.accounts()[0].accountUid(),
-                        roundUpsSavingGoal.savingsGoalUid(), requestBody);
-                log.debug("Add money to saving goal response: " + addMoneyToSavingsGoalResponse);
+                addRoundupAmountToSavingsGoal(primaryAccount.accountUid(), totalRoundup,
+                        roundUpsSavingGoal.savingsGoalUid());
             } else {
                 // create savings goal
-                var savingsGoalRequestBody = new SavingGoalRequestBody(ROUND_UP_SAVINGS_GOAL, "GBP",
-                        new CurrencyAndAmount("GBP", 100000), "string");
-                var createSavingsGoalResponseJson =
-                        apiCalls.sendCreateSavingsGoalRequest(accounts.accounts()[0].accountUid(), savingsGoalRequestBody);
-                var createSavingsGoalResponse = gson.fromJson(createSavingsGoalResponseJson,
-                        CreateOrUpdateSavingsGoalResponse.class);
-                var topUpRequestBody = new TopUpRequestBody(new CurrencyAndAmount("GBP", totalRoundup));
-
-                var addMoneyToSavingsGoalResponse = apiCalls.sendAddMoneyIntoSavingsGoalRequest(accounts.accounts()[0].accountUid(),
-                        createSavingsGoalResponse.savingsGoalUid(), topUpRequestBody);
-                System.out.println("Added " + totalRoundup + "to the Round-ups Savings Goal");
-//                System.out.println("New balance for the savings goal: " + sav);
-                log.debug("Add money to saving goal response: " + addMoneyToSavingsGoalResponse);
+                var createSavingsGoalResponse = createSavingsGoal(primaryAccount.accountUid());
+                addRoundupAmountToSavingsGoal(primaryAccount.accountUid(), totalRoundup,
+                        createSavingsGoalResponse.savingsGoalUid());
             }
+            System.out.println("Added " + totalRoundup + " to the " + ROUND_UP_SAVINGS_GOAL + " Goal");
         } else {
             System.out.println("There was no round-ups to transfer to the Savings Goal");
         }
     }
 
     /**
+     * Sends a GET request to the Starling Bank API to retrieve the list of accounts for our sandbox customer,
+     * coverts the JSON response received from the Starling Bank API to an AccountList object, and then finds the
+     * primary account in the list.
+     *
+     * @return The customer's primary account
+     */
+    private Account retrievePrimaryAccount() {
+        var accountsApiResponse = apiCalls.sendBasicGetRequest(STARLING_API_BASE_URL + "/api/v2/accounts");
+        var accountsList = gson.fromJson(accountsApiResponse, AccountsList.class);
+
+        Account primaryAccount = null;
+
+        for (Account account : accountsList.accounts()) {
+            if (account.accountType().equals("PRIMARY")) {
+                primaryAccount = account;
+            }
+        }
+        return primaryAccount;
+    }
+
+    /**
+     * Sends a GET request to the Starling API to retrieve the transactions since last week for our sandbox customer,
+     * using the accountUid and accountDefaultCategory. The JSON response received is converted to a TransactionFeed
+     * object.
+     *
+     * @param accountUid             The unique identifier for the account from where the transactions should be retrieved
+     * @param accountDefaultCategory The account's default category, which will be used to retrieve the transaction
+     * @return The customer's feed of transactions since last week
+     */
+    private TransactionFeed retrieveLastWeekTransactionFeed(String accountUid, String accountDefaultCategory) {
+        // replicates Starling Bank Format for their Date Time
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+        var lastWeek = OffsetDateTime.now().minusWeeks(1);
+        var lastWeekStarlingBankFormat = lastWeek.withOffsetSameInstant(ZoneOffset.UTC).format(formatter);
+
+        var transactionFeedsUri = STARLING_API_BASE_URL + "/api/v2/feed/account/"
+                + accountUid + "/category/" + accountDefaultCategory
+                + "?changesSince=" + lastWeekStarlingBankFormat;
+        var transactionFeedsResponse = apiCalls.sendBasicGetRequest(transactionFeedsUri);
+        return gson.fromJson(transactionFeedsResponse, TransactionFeed.class);
+    }
+
+    /**
      * Each transaction is rounded up to the closest main unit of the currency (100 minor units), if it isn't
      * already a multiple of 100. All world currency are decimal based, except 2, but these non-decimal
      * minor units are not used so the round up calculation should work for all of them.
+     *
      * @param transactionFeed Feed (list) of transactions
      * @return the total of the round ups on the transactions provided.
      */
@@ -100,6 +137,45 @@ public class RoundupsService {
         return totalRoundup;
     }
 
+    /**
+     * Sends a GET request to retrieve the list of savings goal for our sandbox customer from the Starling Bank
+     * API, and converts the JSON response received to a SavingsGoalList object.
+     *
+     * @param accountUid The unique identifier of the account from where the savings goals are retrieved
+     * @return List of Savings Goals that the customer has
+     */
+    private SavingsGoalList retrieveSavingsGoalList(String accountUid) {
+        var savingGoalsResponse = apiCalls.sendBasicGetRequest(STARLING_API_BASE_URL + "/api/v2/account/"
+                + accountUid + "/savings-goals");
+        return gson.fromJson(savingGoalsResponse, SavingsGoalList.class);
+    }
+
+    /**
+     * Add the round-up amount to the savings goal provided, by sending a PUT request to the Starling Bank API.
+     *
+     * @param accountUid     The unique identifier of the account from where the savings goal is retrieved
+     * @param totalRoundup   The total amount from round-ups that should be added to the savings goal
+     * @param savingsGoalUid The unique identifier of the savings goal to which the totalRoundup will be added
+     */
+    private void addRoundupAmountToSavingsGoal(String accountUid, int totalRoundup, String savingsGoalUid) {
+        var requestBody = new TopUpRequestBody(new CurrencyAndAmount("GBP", totalRoundup));
+        apiCalls.sendAddMoneyIntoSavingsGoalRequest(accountUid, savingsGoalUid, requestBody);
+    }
+
+    /**
+     * Creates the Rounds-up saving goal, by sending a PUT request to the Starling Bank API.
+     *
+     * @param accountUid The unique identifier of the account under which the new savings goal will be created
+     * @return The response from the PUT request that includes the unique identifier for the savings goal that was
+     * created
+     */
+    private CreateOrUpdateSavingsGoalResponse createSavingsGoal(String accountUid) {
+        var savingsGoalRequestBody = new SavingGoalRequestBody(ROUND_UP_SAVINGS_GOAL, "GBP",
+                new CurrencyAndAmount("GBP", 100000), "string");
+        var createSavingsGoalResponseJson =
+                apiCalls.sendCreateSavingsGoalRequest(accountUid, savingsGoalRequestBody);
+        return gson.fromJson(createSavingsGoalResponseJson, CreateOrUpdateSavingsGoalResponse.class);
+    }
 
     /**
      * Checks whether the transaction providing is outgoing, settled and a specific type of payment i.e. master card or
